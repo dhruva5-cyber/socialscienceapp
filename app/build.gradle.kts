0d1b25cca8f299bf52e6b57223c6f81ee7103cb6
Dhruva <deeptidruva684@gmail.com>
2026-07-10 08:36:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     implementation(composeBom)
     androidTestImplementation(composeBom)
-    implementation "com.google.android.material:material:1.12.0"
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
@@ -69,7 +69,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-
+    implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.navigation:navigation-compose:2.9.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.7")
